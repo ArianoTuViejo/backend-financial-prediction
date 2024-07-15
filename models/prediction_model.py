105ff0a6799da22680790039ed3b4794c9954d51
Ariano2700 <kawuai123loco123@gmail.com>
2024-07-15 17:02:19 -0500

    refactor: changes in the promt
    changes in promt and in the way data is received in prediction_model, now it returns prediction for the next two months.

--- models/prediction_model.py
+++ models/prediction_model.py
@@ -43,14 +43,11 @@ class PredictionModel:  # Clase PredictionModel
         for element in data_consulta:
             array_totales.append(element['total'])
 
-        # Obtenemos el mes con mayor cantidad de datos
-        # consult = ("De acuerdo a este arreglo de meses ", str(meses)," tenemos que los ", str(tipo_data_nombre), " una empresa en el año ", str(tipo_anio_nombre), "son estos ",
-        # str(array_totales), " Dime que mes es el que tuvo una mayor cantidad de", str(tipo_data_nombre),"y en un valor aparte dime el monto")
-        # consult = ("De acuerdo a este arreglo de meses ", str(meses)," tenemos que los ", str(tipo_data_nombre), " una empresa en el año ", str(tipo_anio_nombre), "son estos ",
-        # str(array_totales), "devuelve una prediccion con regresión lineal de estos datos de los", str(tipo_data_nombre),"del siguiente mes")
+        # consult = (f"Predice el total de {str(tipo_data_nombre)} para el siguiente mes basado en los datos de {str(tipo_data_nombre)} de los años 2023 y 2024: {
+        #            array_totales}", "Solo necesito que me devuelvas el ingreso total predicho para el siguiente mes, no necesito que me pases más texto, solo el dato final")
 
-        consult = (f"Predice el total de {str(tipo_data_nombre)} para el siguiente mes basado en los datos de {str(tipo_data_nombre)} de los años 2023 y 2024: {
-                   array_totales}", "Solo necesito que me devuelvas el ingreso total predicho para el siguiente mes, no necesito que me pases más texto, solo el dato final")
+        consult = (f"Predice el total de {str(tipo_data_nombre)} para los dos siguientes meses basado en los datos de {str(tipo_data_nombre)} de los años 2023 y 2024: {
+                   array_totales}", "Solo necesito que me devuelvas el ingreso total predicho para el siguiente mes, no necesito que me pases más texto, solo los dos datos finales")
 
         # Imprimimos la consulta
         print("Consulta: ", consult)
@@ -58,12 +55,27 @@ class PredictionModel:  # Clase PredictionModel
         # Genera el contenido con el modelo
         response = self.model.generate_content(consult)
         print("CONSULTA EXITOSA: ")
+        print(response.text)
 
         try:
-            prediction_text = response.text
-            predict_income = float(prediction_text)
+            prediction_text = response.text.strip().split("\n")
+            predictions = [float(prediction.replace(",", "."))
+                           for prediction in prediction_text]
 
-            return {"prediction": predict_income}
+            response_dict = {
+                "predictionOneMonth": predictions[0],
+                "predictionTwoMonth": predictions[1],
+            }
+            return response_dict
         except (ValueError, IndexError) as e:
             print(f"Error al procesar la respuesta del modelo: {e}")
             return {"message": "Error: No se pudo obtener una predicción válida"}
+
+        # try:
+        #     prediction_text = response.text
+        #     predict_income = float(prediction_text)
+
+        #     return {"prediction": predict_income}
+        # except (ValueError, IndexError) as e:
+        #     print(f"Error al procesar la respuesta del modelo: {e}")
+        #     return {"message": "Error: No se pudo obtener una predicción válida"}
